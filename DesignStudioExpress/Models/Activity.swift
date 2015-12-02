eedbbb4c869944ffbaf6d043fa7a7bf52019fbb2
Kristijan Perusko <kjp@alexanderinteractive.com>
2015-12-02 16:49:31 -0500

    #7 Minor layout fixes

--- DesignStudioExpress/Models/Activity.swift
+++ DesignStudioExpress/Models/Activity.swift
@@ -13,7 +13,7 @@ class Activity: Object {
     dynamic var id = NSUUID().UUIDString
     dynamic var title = ""
     dynamic var activityDescription = "" // we can't use just description, because that's an NSObject stored property
-    dynamic var duration: Int = 0
+    dynamic var duration: Int = 5
     dynamic var notes = ""
     dynamic var dateCreated: NSDate = NSDate()
     
@@ -24,7 +24,7 @@ class Activity: Object {
     class func createDefaultActivity() -> Activity {
         let activity = Activity()
         activity.title = "Activity Number One"
-        activity.duration = 0
+        activity.duration = 5
         activity.activityDescription = "Here’s where you describe one step in solving your challenge. Like, \"cardstorm new saving features\", or \"share sketches within mini-groups\"."
         
         return activity
