363550c32f33cf0efc43d30b29e7442bb6f11b5d
Kristijan Perusko <kjp@alexanderinteractive.com>
2015-12-08 19:18:34 -0500

    #22 Fixed the bug where the timer screen didn't show if we were on a Activity detail page

--- DesignStudioExpress/Views/Subviews/UITabBarControllerBase.swift
+++ DesignStudioExpress/Views/Subviews/UITabBarControllerBase.swift
@@ -79,9 +79,22 @@ class UITabBarControllerBase: UITabBarController {
     func showEndActivityScreen(notification: NSNotification) {
         if let vc = self.storyboard?.instantiateViewControllerWithIdentifier(ViewControllerIdentifier.ActivityEndedScreen.rawValue) {
             vc.modalPresentationStyle = UIModalPresentationStyle.OverCurrentContext
-
-            self.presentViewController(vc, animated: true, completion: nil)
+            
+            // if we have a modal that's open, we have to open our modal from that vc
+            let topController = self.findLastPresentedController()
+            topController.presentViewController(vc, animated: true, completion: nil)
         }
+    }
+    
+    // finds the last presented controller in our view hierarchy
+    private func findLastPresentedController() -> UIViewController {
+        var topController = UIApplication.sharedApplication().keyWindow!.rootViewController;
+    
+        while topController?.presentedViewController != nil {
+            topController = topController!.presentedViewController;
+        }
+    
+     	   return topController!
     }
     
     func showTimerScreen(notification: NSNotification) {
@@ -115,6 +128,14 @@ class UITabBarControllerBase: UITabBarController {
     }
     
     func navigateToTimerScreen() {
+        // if the top vc is modal, we need to dismiss it, so that we can show the timer page
+        // TODO we should find a better way to test for this
+        // because if we're adding more modals this will not work
+        let topController = self.findLastPresentedController()
+        if topController is ActivityDetailViewController {
+            topController.dismissViewControllerAnimated(false, completion: nil)
+        }
+        
         let navViewController = self.viewControllers![createDesignStudioNavTabIndex] as! UINavigationController
         let timerVC = navViewController.viewControllers[navViewController.viewControllers.endIndex-1] // endIndex is "past-the-end"
         navigationController?.popToViewController(timerVC, animated: true)
