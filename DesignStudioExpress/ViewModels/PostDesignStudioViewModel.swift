f9807d88bd30cc8f81394d39278a245d6606f4ae
Kristijan Perusko <kjp@alexanderinteractive.com>
2015-12-16 17:19:34 -0500

    #65 Reversed order of images in the gallery so that the last taken image is first in the gallery

--- DesignStudioExpress/ViewModels/PostDesignStudioViewModel.swift
+++ DesignStudioExpress/ViewModels/PostDesignStudioViewModel.swift
@@ -52,7 +52,7 @@ class PostDesignStudioViewModel {
         
         //remove all items when we're refreshing data
         self.data.removeAll()
-
+        var temp = [MHGalleryItem]()
         assetLibrary.enumerateGroupsWithTypes(assetsType, usingBlock: { (group: ALAssetsGroup!, stop:UnsafeMutablePointer<ObjCBool>) -> Void in
             
             guard group != nil && String(group.valueForProperty(ALAssetsGroupPropertyName)) == defaultAlbumName else {
@@ -67,11 +67,17 @@ class PostDesignStudioViewModel {
             group.enumerateAssetsUsingBlock({ (asset: ALAsset!, idx, stop) -> Void in
                 if asset != nil {
                     if let defaultRep = asset.defaultRepresentation() {
-                        self.data.append(MHGalleryItem(URL: defaultRep.url().absoluteString, galleryType: .Image))
+                        // appending and then reversing the order is faster
+                        // than just inserting it at index 0
+                        temp.append(MHGalleryItem(URL: defaultRep.url().absoluteString, galleryType: .Image))
                     }
                 }
+                
                 // check that we loaded all images before calling the callback
                 if idx >= totalImages - 1 {
+                    // reverse the order of items so that latest image is first
+                    self.data = temp.reverse()
+
                     loadFinishedCallback()
                 }
             })
