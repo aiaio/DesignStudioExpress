14791b48d4a0c13175ad35af879769c43634ae49
Kristijan Perusko <kjp@alexanderinteractive.com>
2015-12-18 11:58:33 -0500

    #15 Added first template data

--- DesignStudioExpress/Models/DesignStudio.swift
+++ DesignStudioExpress/Models/DesignStudio.swift
@@ -42,64 +42,165 @@ class DesignStudio: Object {
         
         let ds = DesignStudio()
         ds.title = "New Project Concept"
-        realm.add(ds)
-        
        
         let challenge = Challenge()
         challenge.title = "Individual Sketching, Group Discussion"
-        challenge.challengeDescription  = ""
+        challenge.challengeDescription  = "This round focuses on cardstorming and rapid sketching. Stick to rough sketches that convey your ideas. You will present, critique and refine your ideas after each activity. Good luck and get sketchy!"
         
         let activity1 = Activity()
         activity1.title = "Cardstorming"
-        activity1.activityDescription = ""
-        activity1.notes = ""
-        activity1.duration = 1
+        activity1.activityDescription = "Warmup and cardstorming. Write as many feature ideas as you can on individual stickies. Save features as your idea palette for sketching."
+        activity1.duration = 5
         challenge.activities.append(activity1)
         
         let activity2 = Activity()
         activity2.title = "Draw 5 ideas in 5 minutes"
-        activity2.activityDescription = ""
-        activity2.notes = ""
-        activity2.duration = 1
+        activity2.activityDescription = "Generate 5 ideas in 5 minutes — should be unique concepts, but can be very rough. Give one minute per idea. It’s okay to have fewer than the 5 ideas."
+        activity2.duration = 5
         challenge.activities.append(activity2)
         
         let activity3 = Activity()
         activity3.title = "Share sketches within mini groups"
-        activity3.activityDescription = ""
-        activity3.notes = ""
-        activity3.duration = 1
+        activity3.activityDescription = "Present ideas to the group to share your concepts and get a critique. Write ideas/questions down as notes. Use most of this time to share ideas."
+        activity3.duration = 10
         challenge.activities.append(activity3)
         
         let activity4 = Activity()
         activity4.title = "Sketch"
-        activity4.activityDescription = ""
-        activity4.notes = ""
-        activity4.duration = 1
+        activity4.activityDescription = "Refine your concepts into the top 2 ideas. Incorporate others’ ideas into your designs."
+        activity4.duration = 10
         challenge.activities.append(activity4)
         
         let activity5 = Activity()
         activity5.title = "Share in mini groups and prepare to present"
-        activity5.activityDescription = ""
-        activity5.notes = ""
-        activity5.duration = 1
+        activity5.activityDescription = "Prepare to present your ideas as a group—condense and eliminate as much as possible. Don’t be afraid to throw away ideas, keep the strongest ones."
+        activity5.duration = 10
         challenge.activities.append(activity5)
         
         let activity6 = Activity()
         activity6.title = "Share and discuss"
-        activity6.activityDescription = ""
-        activity6.notes = ""
-        activity6.duration = 1
+        activity6.activityDescription = "Each group presents and receives critiques. Discuss all ideas and determine which ideas people like the best. If you’d like, hold a vote."
+        activity6.duration = 20
         challenge.activities.append(activity6)
         
         let activity7 = Activity()
         activity7.title = "Break"
-        activity7.activityDescription = ""
-        activity7.notes = ""
-        activity7.duration = 1
+        activity7.activityDescription = "If you think people need one! Don’t forget to refresh all your supplies for the next challenge."
+        activity7.duration = 5
         challenge.activities.append(activity7)
         
         ds.challenges.append(challenge)
         
+        // Challenge #2
+        
+        let challenge2 = Challenge()
+        challenge2.title = "Individual and Group Sketching"
+        challenge2.challengeDescription  = "This round focuses on cardstorming and rapid sketching. Stick to rough sketches that convey your ideas. You will present, critique and refine your ideas after each activity. Good luck and get sketchy!"
+        
+        let activity21 = Activity()
+        activity21.title = "Cardstorming"
+        activity21.activityDescription = "Warmup and cardstorming. Write as many feature ideas as you can on individual stickies. Save features as your idea palette for sketching."
+        activity21.duration = 5
+        challenge2.activities.append(activity21)
+        
+        let activity22 = Activity()
+        activity22.title = "Draw 5 ideas in 5 minutes"
+        activity22.activityDescription = "Generate 5 ideas in 5 minutes — should be unique concepts, but can be very rough. Give one minute per idea. It’s okay to have fewer than the 5 ideas."
+        activity22.duration = 5
+        challenge2.activities.append(activity22)
+        
+        let activity23 = Activity()
+        activity23.title = "Share sketches within mini groups"
+        activity23.activityDescription = "Present ideas to the group to share your concepts and get a critique. Write ideas/questions down as notes. Use most of this time to share ideas."
+        activity23.duration = 10
+        challenge2.activities.append(activity23)
+        
+        let activity24 = Activity()
+        activity24.title = "Sketch"
+        activity24.activityDescription = "Refine your concepts into the top 2 ideas. Incorporate others’ ideas into your designs."
+        activity24.duration = 5
+        challenge2.activities.append(activity24)
+        
+        let activity25 = Activity()
+        activity25.title = "Sketch collaboratively in mini groups"
+        activity25.activityDescription = "Together, each mini group sketches, consolidating their ideas into a single vision to answer the challenge."
+        activity25.duration = 10
+        challenge2.activities.append(activity25)
+        
+        let activity26 = Activity()
+        activity26.title = "Share and discuss"
+        activity26.activityDescription = "Each group presents and receives critiques. Discuss all ideas and determine which ideas people like the best. If you’d like, hold a vote."
+        activity26.duration = 20
+        challenge2.activities.append(activity26)
+        
+        let activity27 = Activity()
+        activity27.title = "Break"
+        activity27.activityDescription = "If you think people need one! Don’t forget to refresh all your supplies for the next challenge."
+        activity27.duration = 5
+        challenge2.activities.append(activity27)
+        
+        ds.challenges.append(challenge2)
+        
+        
+        // Challenge #3
+        
+        let challenge3 = Challenge()
+        challenge3.title = "Grouping Similar Ideas"
+        challenge3.challengeDescription  = "This is a cardstorming challenge. Present your best ideas and discuss their value. Then refine and  combine them into themes. Remember — ideas should be mapped to the users’ goals and needs."
+        
+        let activity31 = Activity()
+        activity31.title = "Cardstorming"
+        activity31.activityDescription = "Warmup and cardstorming. Write as many feature ideas as you can on individual stickies. Save features as your idea palette."
+        activity31.duration = 5
+        challenge3.activities.append(activity31)
+        
+        let activity32 = Activity()
+        activity32.title = "Present to mini group"
+        activity32.activityDescription = "Present your feature ideas to the group.  Ideas that have more than one item might be winners and you can start to combine ideas into themes."
+        activity32.duration = 10
+        challenge3.activities.append(activity32)
+        
+        let activity33 = Activity()
+        activity33.title = "Group ideas by common themes"
+        activity33.activityDescription = "Group major themes together. Record any new ideas that come up during grouping and add in new stickies."
+        activity33.duration = 10
+        challenge3.activities.append(activity33)
+        
+        let activity34 = Activity()
+        activity34.title = "Prepare to present"
+        activity34.activityDescription = "Prepare to present your ideas as a group—condense as much as possible. Give each category of stickies a name."
+        activity34.duration = 10
+        challenge3.activities.append(activity34)
+        
+        let activity35 = Activity()
+        activity35.title = "Present and discuss with the full group"
+        activity35.activityDescription = "Each group presents their categories and sub-topics. Group major themes together, other teams can add to existing categories or add new ones."
+        activity35.duration = 20
+        challenge3.activities.append(activity35)
+        
+        ds.challenges.append(challenge3)
+        
+        // Final Challenge 
+        
+        let challenge4 = Challenge()
+        challenge4.title = "Wrapup"
+        challenge4.challengeDescription  = "Discuss and document your work. During discussion think about the strengths and weaknesses from the user’s point of view. Remember to take photos, you don’t want to lose any valuable ideas."
+        
+        let activity41 = Activity()
+        activity41.title = "Final discussion"
+        activity41.activityDescription = "Share final thoughts on the ideas created or the design studio. Consider next steps and how you can each move forward with your new ideas."
+        activity41.duration = 20
+        challenge4.activities.append(activity41)
+        
+        let activity42 = Activity()
+        activity42.title = "Document and save"
+        activity42.activityDescription = "Gather everyone’s notes, stickies, and sketches to refer to as you create higher fidelity designs. Don’t forget to take photos."
+        activity42.duration = 5
+        challenge4.activities.append(activity42)
+        
+        ds.challenges.append(challenge4)
+        
+        // save the DS
         realm.add(ds)
         
         do {
